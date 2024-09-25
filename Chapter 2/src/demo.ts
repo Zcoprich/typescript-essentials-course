4bec3b1ba844086da6aa2e00e403372010c34f5a
Zcoprich <zaria.coprich@gmail.com>
2024-09-25 12:01:48 -0500

    Experimented with generic types

--- Chapter 2/src/demo.ts
+++ Chapter 2/src/demo.ts
@@ -3,9 +3,12 @@ interface Contact{
     name: string;
 }
 
-function clone(source: Contact): Contact{
+function clone<T1, T2>(source: T1): T2{
     return Object.apply({}, source);
 }
 
 const a: Contact = {id: 123, name: "Homer Simpsons"};
-const b = clone(a);
+const b = clone<Contact, Contact>(a);
+
+const dateRange = {startDate: Date.now(), endDate: Date.now()};
+const dateRangeCopy = clone(dateRange);
