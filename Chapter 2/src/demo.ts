6e5c8b20b9f3aedf7de96154b05398ecc2ed39b6
Zcoprich <zaria.coprich@gmail.com>
2024-09-25 11:16:37 -0500

    Simplified Contact interface and created variable demonstrating function use

--- Chapter 2/src/demo.ts
+++ Chapter 2/src/demo.ts
@@ -1,17 +1,11 @@
 interface Contact{
     id: number;
     name: string;
-    birthDate?: Date;
-    status: ContactStatus;
-    clone(name:string): Contact;
-}
-
-enum ContactStatus {
-    Active = "active",
-    Inactive = "inactive",
-    New = "new"
 }
 
 function clone(source: Contact): Contact{
     return Object.apply({}, source);
 }
+
+const a: Contact = {id: 123, name: "Homer Simpsons"};
+const b = clone(a);
